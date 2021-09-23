555f4069043da38790d90af3a168010469be80e0
Loric Brevet <loric.brevet@amersports.com>
2021-09-23 09:19:48 +0200

    Move all web/core relative code in web.go

--- web.go
+++ web.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"html/template"
 	"io/fs"
 	"net/http"
 	"path/filepath"
+	"runtime/debug"
 )
 
 // templateData contains all kinds of objects
@@ -169,4 +171,63 @@ func (app *application) renderPartial(w http.ResponseWriter, r *http.Request, na
 		app.serverError(w, err)
 		return
 	}
+}
+
+// logRequest is a middleware that logs request to the application logger.
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.logger.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	})
+}
+
+// secureHeaders is a middleware that injects headers in the response
+// to prevent XSS and Clickjacking attacks.
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Frame-Options", "deny")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// recoverPanic gracefully handles any panic that happens in the current go routine.
+// By default, panics don't shut the entire application (only the current go routine),
+// but if one arise, the server will return an empty response. This middleware is taking
+// care of recovering the panic and sending a regular 500 server error.
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				// make the http.Server automatically close the current connection.
+				w.Header().Set("Connection", "close")
+
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// The serverError helper writes an error message and stack trace to the errorLog,
+// then sends a generic 500 Internal Server Error response to the user.
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.logger.Output(2, trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// The clientError helper sends a specific status code and corresponding description
+// to the user. This should be used to send responses when there's a problem with the
+// request that the user sent.
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// The flash helper will add a global flash message.
+func (app *application) flash(r *http.Request, msg string) {
+	app.session.Put(r, "flash", app.translator.translate(msg))
 }
